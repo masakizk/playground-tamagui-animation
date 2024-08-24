5a71fe13092de5ce0651ebe9abf4d46e66477375
zacker <iszkmasaki@gmail.com>
2024-08-24 20:45:51 +0900

    feat: dialogを追加

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,5 +1,5 @@
 import { useColorScheme } from 'react-native'
-import { TamaguiProvider, type TamaguiProviderProps } from 'tamagui'
+import { PortalProvider, TamaguiProvider, type TamaguiProviderProps } from 'tamagui'
 import { ToastProvider, ToastViewport } from '@tamagui/toast'
 import { CurrentToast } from './CurrentToast'
 import { config } from '../tamagui.config'
@@ -23,7 +23,9 @@ export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'conf
           ]
         }
       >
-        {children}
+        <PortalProvider shouldAddRootHost>
+            {children}
+        </PortalProvider> 
         <CurrentToast />
         <ToastViewport top="$8" left={0} right={0} />
       </ToastProvider>
--- app/index.tsx
+++ app/index.tsx
@@ -1,48 +1,48 @@
-import { ExternalLink } from '@tamagui/lucide-icons'
-import { Anchor, H2, Paragraph, XStack, YStack } from 'tamagui'
+import { Text, Button, H2, YStack, Sheet } from 'tamagui'
 import { ToastControl } from 'app/CurrentToast'
+import { useState } from 'react'
 
 export default function TabOneScreen() {
   return (
     <YStack f={1} ai="center" gap="$8" px="$10" pt="$5">
-      <H2>Tamagui + Expo</H2>
-
+      <H2>Tamagui Animation</H2>
       <ToastControl />
-
-      <XStack ai="center" jc="center" fw="wrap" gap="$1.5" pos="absolute" b="$8">
-        <Paragraph fos="$5">Add</Paragraph>
-
-        <Paragraph fos="$5" px="$2" py="$1" col="$blue10" bg="$blue5" br="$3">
-          tamagui.config.ts
-        </Paragraph>
-
-        <Paragraph fos="$5">to root and follow the</Paragraph>
-
-        <XStack
-          ai="center"
-          gap="$1.5"
-          px="$2"
-          py="$1"
-          br="$3"
-          bg="$purple5"
-          hoverStyle={{ bg: '$purple6' }}
-          pressStyle={{ bg: '$purple4' }}
-        >
-          <Anchor
-            href="https://tamagui.dev/docs/core/configuration"
-            textDecorationLine="none"
-            col="$purple10"
-            fos="$5"
-          >
-            Configuration guide
-          </Anchor>
-          <ExternalLink size="$1" col="$purple10" />
-        </XStack>
-
-        <Paragraph fos="$5" ta="center">
-          to configure your themes and tokens.
-        </Paragraph>
-      </XStack>
-    </YStack>
+      <BottomSheet />
+    </YStack >
   )
+}
+
+
+function BottomSheet() {
+  const [isVisible, setIsVisible] = useState(false)
+  return <>
+    <Button ai="center" onPress={() => setIsVisible(true)}>
+      <Text>Show Dialog</Text>
+    </Button>
+    <Sheet
+      animation="medium"
+      open={isVisible}
+      snapPoints={["80%"]}
+      snapPointsMode='mixed'
+      dismissOnSnapToBottom={true}
+      dismissOnOverlayPress={true}
+      disableDrag={false}
+      forceRemoveScrollEnabled={isVisible}
+      onOpenChange={setIsVisible}
+    >
+      <Sheet.Overlay
+        animation="slow"
+        enterStyle={{ opacity: 0 }}
+        exitStyle={{ opacity: 0 }}
+      />
+      <Sheet.Frame
+        backgroundColor="$gray10"
+        w="100%"
+        ai="center"
+        jc="center"
+      >
+        <Text>Dialog</Text>
+      </Sheet.Frame>
+    </Sheet>
+  </>
 }
